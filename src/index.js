1fd9dd42eedf61b60fe868b36934badce00d1d3c
javohir <ruzikulovjavokhir005@gmail.com>
2023-10-16 11:56:09 +0500

    alaliy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import Provider from './context';
 import './index.css';
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider>
